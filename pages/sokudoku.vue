6aa4a6981c1194555f0c7f1fa7f0cd60a5d371d8
fumiyadoi <38125676+fumiyadoi@users.noreply.github.com>
2022-01-10 23:24:31 +0900

    0.0.31

--- pages/sokudoku.vue
+++ pages/sokudoku.vue
@@ -3,7 +3,7 @@
     <div class="column is-4-tablet is-11-mobile">
       <div class="columns is-mobile is-multiline is-centered is-vcentered">
         <div class="column is-12 wrapper-content mb-5 tategaki-wrapper">
-          <div class="pt-3 pb-1" :class="fontsize">{{text}}</div>
+          <div class="tategaki pt-3 pb-1" :class="fontsize">{{text}}</div>
           <div class="is-size-7 has-text-centered">{{(page + 1) +'/'+length}}</div>
         </div>
         <div class="column is-12 wrapper-content">
@@ -95,6 +95,7 @@ export default {
     }
     /* 全ページ数と現在ページの表示を設定します。 */
     this.page = this.$store.state.data.bookPages[this.$store.state.data.bookIndex][0]
+    this.text = this.page
     this.length = this.trimmed.length
     this.$nuxt.$emit('updateRef', 'sokudoku-top')/* navbarの戻るボタンの遷移先の受け渡し */
     this.$nuxt.$emit('updateTitle', this.bookTitle)/* navbarのタイトルの受け渡し */
@@ -127,6 +128,7 @@ export default {
       this.pause()
       this.$store.commit('data/goTopPage', 0)
       this.page = this.$store.state.data.bookPages[this.$store.state.data.bookIndex][0]
+      this.text = this.page
     },
     pause () {
       clearInterval(this.intervalId)
@@ -138,7 +140,7 @@ export default {
         if (this.$store.state.data.bookPages[this.$store.state.data.bookIndex][0] < this.length - 1) {
           this.$store.commit('data/changePage', 0)
           this.page = this.$store.state.data.bookPages[this.$store.state.data.bookIndex][0]
-          this.text = String(this.page)
+          this.text = this.page
         } else {
           this.pause()
         }
